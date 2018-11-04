fc4cd9f28866805ad6724df8f19afa1ccc439550
Jakub Janca <janca.jake@gmail.com>
2018-11-04 20:51:05 +0100

    Updates

---
+++ precache-manifest.4c58d57bbddbdca321c4f6b82d41ed74.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "839f900185b8671a4322018198143137",
+    "url": "/ToDoList/static/media/SpicyRice-Regular.839f9001.ttf"
+  },
+  {
+    "revision": "b0420226dc1dc1a6d39c",
+    "url": "/ToDoList/static/js/runtime~main.b0420226.js"
+  },
+  {
+    "revision": "efb8b99cef0ec1e88646",
+    "url": "/ToDoList/static/js/main.efb8b99c.chunk.js"
+  },
+  {
+    "revision": "3af57877e8ba1eba1ee5",
+    "url": "/ToDoList/static/js/1.3af57877.chunk.js"
+  },
+  {
+    "revision": "efb8b99cef0ec1e88646",
+    "url": "/ToDoList/static/css/main.1e5a5e02.chunk.css"
+  },
+  {
+    "revision": "3af57877e8ba1eba1ee5",
+    "url": "/ToDoList/static/css/1.7f595dc2.chunk.css"
+  },
+  {
+    "revision": "e2dec3c0924531b26e91c9a419b9a00c",
+    "url": "/ToDoList/index.html"
+  }
+];
